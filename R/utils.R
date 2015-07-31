dfcec50f9620ff7d25ae75d7da96cd32cca07aeb
Long Qu <long.qu@wright.edu>
2015-07-31 11:17:05 -0400

    removed duplicated utils.R

--- R/utils.R
+++ R/utils.R
@@ -1,59 +1,77 @@
 safeseq = function (from = 1L, to = 1L, by = 1L, ...) 
 {
-    disc = by * (from - to)
-    if (disc > 0) {
-        vector(class(disc), 0L)
-    }
-    else seq(from = from, to = to, by = by, ...)
+  disc = by * (from - to)
+  if (disc > 0) {
+    vector(class(disc), 0L)
+  }
+  else seq(from = from, to = to, by = by, ...)
 }
 
+defaultBits=128L
 bz0 = as.bigz(0L)
 bq0 = as.bigq(0L)
 bz1 = as.bigz(1L)
 bq1 = as.bigq(1L)
 bzn1 = as.bigz(-1L)
 bqn1 = as.bigq(-1L)
+bf0 = mpfr(0L,defaultBits)
+bf1 = mpfr(1L,defaultBits)
+bfn1 = mpfr(-1L,defaultBits)
 trimZeros = function(x, end='trailing', empty.OK=TRUE) UseMethod('trimZeros')
 trimZeros.default=function(x, end='trailing', empty.OK=TRUE)
 {
-	end = match.arg(end, c('leading', 'trailing', 'both'))
-	trailing = any(end==c('trailing','both'))
-	
-	if(trailing) x=rev(x)
-	nzeros=sum(cumsum(abs(x))==0)
-    ans = if(nzeros>0) x[-seq(nzeros)] else x
-	if(!empty.OK && length(ans)==0L) ans = 0
-	if(trailing) ans = rev(ans)
-	if(end=='both') Recall(ans, 'leading', empty.OK) else ans
+  end = match.arg(end, c('leading', 'trailing', 'both'))
+  trailing = any(end==c('trailing','both'))
+  
+  if(trailing) x=rev(x)
+  nzeros=sum(cumsum(abs(x))==0)
+  ans = if(nzeros>0) x[-seq(nzeros)] else x
+  if(!empty.OK && length(ans)==0L) ans = 0
+  if(trailing) ans = rev(ans)
+  if(end=='both') Recall(ans, 'leading', empty.OK) else ans
 }
 trimZeros.bigz=function(x, end='trailing', empty.OK=TRUE)
 {
-	end = match.arg(end, c('leading', 'trailing', 'both'))
-	trailing = any(end==c('trailing','both'))
-
-	if(trailing) x=rev(x)
-	nzeros=sum(cumsum(abs(x))==bz0)
-    ans = if(nzeros>0) x[-seq(nzeros)] else x
-	if(!empty.OK && length(ans)==0L) ans = bz0
-	if(trailing) ans = rev(ans)
-	if(end=='both') Recall(ans, 'leading', empty.OK) else ans
+  end = match.arg(end, c('leading', 'trailing', 'both'))
+  trailing = any(end==c('trailing','both'))
+  
+  if(trailing) x=rev(x)
+  nzeros=sum(cumsum(abs(x))==bz0)
+  ans = if(nzeros>0) x[-seq(nzeros)] else x
+  if(!empty.OK && length(ans)==0L) ans = bz0
+  if(trailing) ans = rev(ans)
+  if(end=='both') Recall(ans, 'leading', empty.OK) else ans
 }
 trimZeros.bigq=function(x, end='trailing', empty.OK=TRUE)
 {
-	end = match.arg(end, c('leading', 'trailing', 'both'))
-	trailing = any(end==c('trailing','both'))
-
-	if(trailing) x=rev(x)
-	nzeros=sum(cumsum(abs(x))==bq0)
-    ans = if(nzeros>0) x[-seq(nzeros)] else x
-	if(!empty.OK && length(ans)==0L) ans = bq0
-	if(trailing) ans = rev(ans)
-	if(end=='both') Recall(ans, 'leading', empty.OK) else ans
+  end = match.arg(end, c('leading', 'trailing', 'both'))
+  trailing = any(end==c('trailing','both'))
+  
+  if(trailing) x=rev(x)
+  nzeros=sum(cumsum(abs(x))==bq0)
+  ans = if(nzeros>0) x[-seq(nzeros)] else x
+  if(!empty.OK && length(ans)==0L) ans = bq0
+  if(trailing) ans = rev(ans)
+  if(end=='both') Recall(ans, 'leading', empty.OK) else ans
+}
+trimZeros.mpfr=function(x, end='trailing', empty.OK=TRUE)
+{
+  end = match.arg(end, c('leading','trailing','both'))
+  trailing = any(end==c('trailing','both'))
+  
+  if(trailing) x=rev(x)
+  nzeros=sum(cumsum(abs(x))==bf0)
+  ans = if(nzeros>0) x[-seq(nzeros)] else x
+  if(!empty.OK && length(ans)==0L) ans = bf0
+  if(trailing) ans = rev(ans)
+  if(end=='both') Recall(ans, 'leading', empty.OK) else ans
+  
 }
 
 trimZeros.polynomialz = function(x, end='trailing', empty.OK=TRUE) 
-	polynomialz(trimZeros(coef(x), end, empty.OK))
-	
-trimZeros.polynomialq = function(x, end='trailing', empty.OK=TRUE) 
-	polynomialq(trimZeros(coef(x), end, empty.OK))
+  polynomialz(trimZeros(coef(x), end, empty.OK))
 
+trimZeros.polynomialq = function(x, end='trailing', empty.OK=TRUE) 
+  polynomialq(trimZeros(coef(x), end, empty.OK))
+trimZeros.polynomialf = function(x, end='trailing', empty.OK=TRUE) 
+  polynomialf(trimZeros(coef(x), end, empty.OK))
