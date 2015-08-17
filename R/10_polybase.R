73bb075f9b4dc601e6e8bfba6ef239ace43c894a
Yixuan Qiu <yixuan.qiu@cos.name>
2015-08-17 12:37:20 -0400

    basic arithmetic operations

--- R/10_polybase.R
+++ R/10_polybase.R
@@ -8,3 +8,53 @@ polynomial = function(coef)
 {
     new("bigPoly", coef = trimZeros(coef, 'trailing', empty.OK = FALSE))
 }
+
+## Univariate arithmetic operations
+setMethod("+", signature(e1 = "bigPoly", e2 = "missing"),
+          function(e1, e2) {
+              e1
+          }
+)
+setMethod("-", signature(e1 = "bigPoly", e2 = "missing"),
+          function(e1, e2) {
+              polynomial(getZero(e1@coef) - e1@coef)
+          }
+)
+## Operations with scalar
+setMethod("*", signature(e1 = "bigPoly", e2 = "coef_type"),
+          function(e1, e2) {
+              polynomial(e1@coef * e2)
+          }
+)
+setMethod("*", signature(e1 = "coef_type", e2 = "bigPoly"),
+          function(e1, e2) {
+              polynomial(e2@coef * e1)
+          }
+)
+setMethod("/", signature(e1 = "bigPoly", e2 = "coef_type"),
+          function(e1, e2) {
+              polynomial(e1@coef / e2)
+          }
+)
+## Operations between polynomials
+setMethod("+", signature(e1 = "bigPoly", e2 = "bigPoly"),
+          function(e1, e2) {
+              l1 = length(e1@coef)
+              l2 = length(e2@coef)
+              if(l1 >= l2)
+              {
+                  e1@coef[1:l2] = e1@coef[1:l2] + e2@coef
+                  return(polynomial(e1@coef))
+              } else {
+                  e2@coef[1:l1] = e2@coef[1:l1] + e1@coef
+                  return(polynomial(e2@coef))
+              }
+          }
+)
+setMethod("-", signature(e1 = "bigPoly", e2 = "bigPoly"),
+          function(e1, e2) {
+              e1 + (-e2)
+          }
+)
+
+
