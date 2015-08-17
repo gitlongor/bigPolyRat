32c8f7a228c9897138c1d433d8b3cf90a1ba4a8a
Yixuan Qiu <yixuan.qiu@cos.name>
2015-08-17 13:18:08 -0400

    testing code

---
+++ R/99_tests.R
@@ -0,0 +1,22 @@
+x = c(0, 0, 2, 3, 4, 5, 0, 0, 1, 0)
+xz = as.bigz(x)
+xq = as.bigq(x)
+xf = mpfr(x, 128L)
+
+p = list(
+    polynomial(x),
+    polynomial(xz),
+    polynomial(xq),
+    polynomial(xf)
+)
+
+lapply(p[1:3], function(e) {
+    +e
+    -e
+    2 * e
+    e * 2
+    e / 2
+    e + e
+    e - e
+    e * e
+})
